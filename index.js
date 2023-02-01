76dca55a4673e94dcda4263682ec03e3817bf51a
Willian Arsenio de Sousa <willian.arsenio.de.sousa@zalando.de>
2023-02-01 21:10:58 +0100

    nao lembro o que eu fiz

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParse = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParse = require("body-parser");
 const app = express();
-const db = require('./queries');
-const https = require('https');
-const fs = require('fs');
+const db = require("./queries");
+const https = require("https");
+const fs = require("fs");
 
 const port = 3000;
 
 app.use(cors());
 
-app.use(bodyParse.json())
+app.use(bodyParse.json());
 app.use(
-    bodyParse.urlencoded({
-        extended: true,
-    })
+  bodyParse.urlencoded({
+    extended: true,
+  })
 );
 
-app.get('/', (request, response) => {
-    response.json({info: 'Node.js, Express, and Postgres API'})
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
 });
 
-app.get('/pessoas',db.getPessoas)
-app.get('/pessoas/:cpf',db.getPessoasByCpf)
-app.post('/pessoas',db.createPessoas)
-app.put('/pessoas/:id',db.updatePessoas)
-app.delete('/pessoas/:id',db.deletePessoas)
+app.get("/pessoas", db.getPessoas);
+app.get("/pessoas/:cpf", db.getPessoasByCpf);
+app.post("/pessoas", db.createPessoas);
+app.put("/pessoas/:id", db.updatePessoas);
+app.delete("/pessoas/:id", db.deletePessoas);
 
-/*app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})*/
-
-https.createServer({
-    key:fs.readFileSync('./privado.pem'),
-    cert: fs.readFileSync('./publico.pem')
-},app).listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
-
-
+https
+  .createServer(
+    {
+      key: fs.readFileSync("./privado.pem"),
+      cert: fs.readFileSync("./publico.pem"),
+    },
+    app
+  )
+  .listen(port, () => {
+    console.log(`App running on port ${port}.`);
+  });
--- queries.js
+++ queries.js
@@ -1,163 +1,135 @@
-const {
-    request,
-    response
-} = require('express')
+const { request, response } = require("express");
 
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 const pool = new Pool({
-    user: 'me',
-    host: 'localhost',
-    database: 'api',
-    password: 'password',
-    port: 5432
-})
+  user: "postgres",
+  host: "localhost",
+  database: "api",
+  password: "postgrespw",
+  port: 32768,
+});
 
 const getPessoas = (request, response) => {
+  const { query } = request;
 
-    const {
-        query
-    } = request;
+  const { nome, cpf } = query;
 
-    const {
-        nome,
-        cpf
-    } = query;
+  let dbquery = "SELECT * FROM pessoas";
+  let dbparams = [];
 
-    let dbquery = 'SELECT * FROM pessoas';
-    let dbparams = [];
+  if (nome && cpf) {
+    dbquery += " WHERE (cpf LIKE $1) AND (nome LIKE $2)";
+    dbparams = [`${cpf}%`, `${nome}%`];
+  } else if (nome && !cpf) {
+    dbquery += " WHERE (nome LIKE $1)";
+    dbparams = [`${nome}%`];
+  } else if (!nome && cpf) {
+    dbquery += " WHERE (cpf LIKE $1)";
+    dbparams = [`${cpf}%`];
+  } else {
+    dbquery += "";
+    dbparams = [];
+  }
 
-    if (nome && cpf) {
-        dbquery += " WHERE (cpf LIKE $1) AND (nome LIKE $2)";
-        dbparams = [`${cpf}%`, `${nome}%`];
-    } else if (nome && !cpf) {
-        dbquery += " WHERE (nome LIKE $1)";
-        dbparams = [`${nome}%`];
-    } else if (!nome && cpf) {
-        dbquery += " WHERE (cpf LIKE $1)";
-        dbparams = [`${cpf}%`];
-    } else {
-        dbquery += "";
-        dbparams = [];
+  dbquery += " ORDER BY id ASC";
+
+  pool.query(dbquery, dbparams, (error, results) => {
+    if (error) {
+      throw error;
     }
 
-    dbquery += " ORDER BY id ASC";
-
-    pool.query(
-        dbquery,
-        dbparams,
-        (error, results) => {
-
-            if (error) {
-                throw error;
-            }
-
-            response.status(200).json(results.rows);
-        }
-    );
-}
+    response.status(200).json(results.rows);
+  });
+};
 
 const getPessoasByCpf = (request, response) => {
+  const { cpf } = request.params;
 
-    const {
-        cpf
-    } = request.params;
-
-    pool.query(`SELECT * FROM pessoas WHERE cpf LIKE $1`, [`${cpf}%`], (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-
-}
+  pool.query(
+    `SELECT * FROM pessoas WHERE cpf LIKE $1`,
+    [`${cpf}%`],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
+    }
+  );
+};
 
 const getPessoasByNome = (request, response) => {
+  const nome = request.params.nome;
 
-    const nome = request.params.nome
-
-    console.log(nome)
-
-    pool.query(`SELECT * FROM pessoas WHERE nome LIKE $1`, [`${nome}%`], (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
+  pool.query(
+    `SELECT * FROM pessoas WHERE nome LIKE $1`,
+    [`${nome}%`],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
+    }
+  );
+};
 
 const createPessoas = (request, response) => {
-    const {
-        nome,
-        cpf,
-        email,
-        logradouro,
-        numero,
-        complemento,
-        cep
-    } = request.body
+  const { nome, cpf, email, logradouro, numero, complemento, cep } =
+    request.body;
 
-    pool.query('INSERT INTO pessoas ( nome, cpf, email, logradouro, numero, complemento, cep) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *', [nome, cpf, email, logradouro, numero, complemento, cep], (error, results) => {
-
-        if (error) {
-
-            throw error
-
-        }
-        // `User added with ID: ${results.rows[0].id}`
-        response.status(201).send({
-            success: true
-        })
-    })
-}
+  pool.query(
+    "INSERT INTO pessoas ( nome, cpf, email, logradouro, numero, complemento, cep) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+    [nome, cpf, email, logradouro, numero, complemento, cep],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      // `User added with ID: ${results.rows[0].id}`
+      response.status(201).send({
+        success: true,
+      });
+    }
+  );
+};
 
 const updatePessoas = (request, response) => {
-    const id = request.params.id
+  const id = request.params.id;
 
-    const {
-        nome,
-        cpf,
-        email,
-        logradouro,
-        numero,
-        complemento,
-        cep
-    } = request.body
+  const { nome, cpf, email, logradouro, numero, complemento, cep } =
+    request.body;
 
-    pool.query(
-        'UPDATE pessoas SET nome = $1, cpf = $2, email = $3, logradouro = $4, numero = $5, complemento = $6, cep = $7 WHERE id = $8',
-        [nome, cpf, email, logradouro, numero, complemento, cep, id],
-        (error, results) => {
-            if (error) {
-                throw error
-            }
+  pool.query(
+    "UPDATE pessoas SET nome = $1, cpf = $2, email = $3, logradouro = $4, numero = $5, complemento = $6, cep = $7 WHERE id = $8",
+    [nome, cpf, email, logradouro, numero, complemento, cep, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
 
-            response.status(200).send({
-                success: true
-            });
-        }
-    )
-}
-
+      response.status(200).send({
+        success: true,
+      });
+    }
+  );
+};
 
 const deletePessoas = (request, response) => {
-    const cpf = request.params.cpf
-    parseInt(cpf);
-    pool.query('DELETE FROM pessoas WHERE id = $1', [cpf], (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).send({
-            success: true
-        });
-    })
-}
-
+  const cpf = request.params.cpf;
+  parseInt(cpf);
+  pool.query("DELETE FROM pessoas WHERE id = $1", [cpf], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send({
+      success: true,
+    });
+  });
+};
 
 module.exports = {
-    getPessoas,
-    getPessoasByCpf,
-    getPessoasByNome,
-    createPessoas,
-    updatePessoas,
-    deletePessoas
-}
+  getPessoas,
+  getPessoasByCpf,
+  getPessoasByNome,
+  createPessoas,
+  updatePessoas,
+  deletePessoas,
+};
